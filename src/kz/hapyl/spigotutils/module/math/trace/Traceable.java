2e5bc35ba360748dba3286feb1d8fa62de4acd8b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-09 21:45:56 +0600

    Fix exceptions.

---
+++ src/kz/hapyl/spigotutils/module/math/trace/Traceable.java
@@ -0,0 +1,38 @@
+package kz.hapyl.spigotutils.module.math.trace;
+
+import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface Traceable {
+
+	void trace();
+
+	static List<LivingEntity> getNearbyLivingEntities(Location location, double radius) {
+		return getNearbyLivingEntities(location, radius, null);
+	}
+
+	static List<LivingEntity> getNearbyLivingEntities(Location location, double radius, @Nullable Predicate<LivingEntity> predicate) {
+		List<LivingEntity> entities = new ArrayList<>();
+		if (location.getWorld() == null) {
+			return entities;
+		}
+
+		location.getWorld()
+				.getNearbyEntities(
+						location,
+						radius,
+						radius,
+						radius,
+						entity -> entity instanceof LivingEntity living && (predicate == null || predicate.test(living))
+				)
+				.forEach(living -> entities.add((LivingEntity)living));
+
+		return entities;
+	}
+
+}
