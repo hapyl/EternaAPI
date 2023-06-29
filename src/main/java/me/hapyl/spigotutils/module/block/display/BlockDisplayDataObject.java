43624b0ebbe7e24972bd6235409193049c8a3c04
hapyl <deojoff@gmail.com>
2023-06-30 02:07:01 +0600

    Update BlockStudioParser

--- src/main/java/me/hapyl/spigotutils/module/block/display/BlockDisplayDataObject.java
+++ src/main/java/me/hapyl/spigotutils/module/block/display/BlockDisplayDataObject.java
@@ -1,6 +1,9 @@
 package me.hapyl.spigotutils.module.block.display;
 
+import me.hapyl.spigotutils.module.entity.Entities;
+import org.bukkit.Location;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Display;
 import org.joml.Matrix4f;
 
 import javax.annotation.Nonnull;
@@ -8,53 +11,16 @@ import javax.annotation.Nonnull;
 /**
  * Represents a block display data object.
  */
-public class BlockDisplayDataObject {
-
-    @Nonnull
-    private BlockData blockData;
-    @Nonnull
-    private Matrix4f matrix;
-
+public final class BlockDisplayDataObject extends DisplayDataObject<BlockData> {
     public BlockDisplayDataObject(@Nonnull BlockData blockData, @Nonnull Matrix4f matrix) {
-        this.blockData = blockData;
-        this.matrix = matrix;
+        super(blockData, matrix);
     }
 
-    /**
-     * Gets the block data.
-     *
-     * @return block data.
-     */
-    @Nonnull
-    public BlockData getBlockData() {
-        return blockData;
-    }
-
-    /**
-     * Sets the block data.
-     *
-     * @param blockData - New block data.
-     */
-    public void setBlockData(@Nonnull BlockData blockData) {
-        this.blockData = blockData;
-    }
-
-    /**
-     * Gets the matrix.
-     *
-     * @return the matrix.
-     */
-    @Nonnull
-    public Matrix4f getMatrix() {
-        return matrix;
-    }
-
-    /**
-     * Sets the matrix.
-     *
-     * @param matrix - New matrix.
-     */
-    public void setMatrix(@Nonnull Matrix4f matrix) {
-        this.matrix = matrix;
+    @Override
+    public Display create(@Nonnull Location location) {
+        return Entities.BLOCK_DISPLAY.spawn(location, self -> {
+            self.setBlock(object);
+            self.setTransformationMatrix(matrix4f);
+        });
     }
 }
---
+++ src/main/java/me/hapyl/spigotutils/module/block/display/DisplayDataObject.java
@@ -0,0 +1,26 @@
+package me.hapyl.spigotutils.module.block.display;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Display;
+import org.joml.Matrix4f;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Represents a base display data object.
+ * Display data objects hold key information on how to spawn display entity.
+ */
+public abstract class DisplayDataObject<T> {
+
+    @Nonnull
+    protected final T object;
+    @Nonnull
+    protected final Matrix4f matrix4f;
+
+    public DisplayDataObject(@Nonnull T object, @Nonnull Matrix4f matrix4f) {
+        this.object = object;
+        this.matrix4f = matrix4f;
+    }
+
+    public abstract Display create(@Nonnull Location location);
+}
