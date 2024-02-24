3d30dfc71f0c4ed8a4b2cf306a0c59712129a42b
hapyl <deojoff@gmail.com>
2024-02-24 17:01:34 +0600

    Rewrite CenterChat.

--- src/main/java/me/hapyl/spigotutils/module/chat/CenterChat.java
+++ src/main/java/me/hapyl/spigotutils/module/chat/CenterChat.java
@@ -6,6 +6,9 @@ import net.md_5.bungee.api.chat.HoverEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * This util class is used to send perfectly centered messages in chat.
  * <p>
@@ -17,7 +20,17 @@ import org.bukkit.entity.Player;
  */
 public class CenterChat {
 
-    private final static int CENTER_PX = 154;
+    /**
+     * The center chat pixel.
+     * (By the original author.)
+     */
+    public final static int CENTER_PX = 154;
+
+    /**
+     * The center motD pixel.
+     * (By hapyl via eyes.)
+     */
+    public final static int CENTER_PX_MOTD = 126;
 
     /**
      * Send a centered message to a player.
@@ -25,11 +38,19 @@ public class CenterChat {
      * @param player  - Player.
      * @param message - Message to center and send.
      */
-    public static void sendCenteredMessage(Player player, String message) {
+    public static void sendCenteredMessage(@Nonnull Player player, @Nonnull String message) {
         player.sendMessage(makeString(message));
     }
 
-    public static void sendCenteredClickableMessage(Player player, String message, HoverEvent hover, ClickEvent click) {
+    /**
+     * Sends a centered clickable and hoverable message to a player.
+     *
+     * @param player  - Player.
+     * @param message - Message.
+     * @param hover   - Hover event.
+     * @param click   Click event.
+     */
+    public static void sendCenteredClickableMessage(@Nonnull Player player, @Nonnull String message, @Nullable HoverEvent hover, @Nullable ClickEvent click) {
         final ComponentBuilder builder = new ComponentBuilder(makeString(message));
         if (hover != null) {
             builder.event(hover);
@@ -40,11 +61,44 @@ public class CenterChat {
         player.spigot().sendMessage(builder.create());
     }
 
-    public static String makeString(String message, int centerPx) {
-        if (message == null || message.equals("")) {
+    /**
+     * Creates a MotD centered message.
+     *
+     * @param message - Message to center.
+     * @return a centered motD message.
+     * @see #CENTER_PX_MOTD
+     */
+    @Nonnull
+    public static String makeMotD(@Nonnull String message) {
+        return makeString(message, CENTER_PX_MOTD);
+    }
+
+    /**
+     * Creates a centered message with a default center px.
+     *
+     * @param message - Message to center.
+     * @return a centered message.
+     * @see #CENTER_PX
+     */
+    @Nonnull
+    public static String makeString(@Nonnull String message) {
+        return makeString(message, CENTER_PX);
+    }
+
+    /**
+     * Creates a centered message with a custom center px.
+     *
+     * @param message  - Message.
+     * @param centerPx - Center px.
+     * @return a centered message.
+     */
+    @Nonnull
+    public static String makeString(@Nonnull String message, int centerPx) {
+        if (message.isEmpty()) {
             return "";
         }
 
+        final MinecraftFont font = MinecraftFont.defaultFont;
         message = ChatColor.translateAlternateColorCodes('&', message);
 
         int messagePxSize = 0;
@@ -60,15 +114,14 @@ public class CenterChat {
                 isBold = c == 'l' || c == 'L';
             }
             else {
-                CenterChat.DefaultFontInfo dFI = CenterChat.DefaultFontInfo.getDefaultFontInfo(c);
-                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
+                messagePxSize += font.getCharLength(c, isBold);
                 messagePxSize++;
             }
         }
 
         int halvedMessageSize = messagePxSize / 2;
         int toCompensate = centerPx - halvedMessageSize;
-        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+        int spaceLength = MinecraftFont.spaceLength + 1;
         int compensated = 0;
         StringBuilder sb = new StringBuilder();
         while (compensated < toCompensate) {
@@ -79,140 +132,4 @@ public class CenterChat {
         return sb + message;
     }
 
-    public static String makeString(String message) {
-        return makeString(message, CENTER_PX);
-    }
-
-    public enum DefaultFontInfo {
-        A('A', 5),
-        a('a', 5),
-        B('B', 5),
-        b('b', 5),
-        C('C', 5),
-        c('c', 5),
-        D('D', 5),
-        d('d', 5),
-        E('E', 5),
-        e('e', 5),
-        F('F', 5),
-        f('f', 4),
-        G('G', 5),
-        g('g', 5),
-        H('H', 5),
-        h('h', 5),
-        I('I', 3),
-        i('i', 1),
-        J('J', 5),
-        j('j', 5),
-        K('K', 5),
-        k('k', 4),
-        L('L', 5),
-        l('l', 1),
-        M('M', 5),
-        m('m', 5),
-        N('N', 5),
-        n('n', 5),
-        O('O', 5),
-        o('o', 5),
-        P('P', 5),
-        p('p', 5),
-        Q('Q', 5),
-        q('q', 5),
-        R('R', 5),
-        r('r', 5),
-        S('S', 5),
-        s('s', 5),
-        T('T', 5),
-        t('t', 4),
-        U('U', 5),
-        u('u', 5),
-        V('V', 5),
-        v('v', 5),
-        W('W', 5),
-        w('w', 5),
-        X('X', 5),
-        x('x', 5),
-        Y('Y', 5),
-        y('y', 5),
-        Z('Z', 5),
-        z('z', 5),
-        NUM_1('1', 5),
-        NUM_2('2', 5),
-        NUM_3('3', 5),
-        NUM_4('4', 5),
-        NUM_5('5', 5),
-        NUM_6('6', 5),
-        NUM_7('7', 5),
-        NUM_8('8', 5),
-        NUM_9('9', 5),
-        NUM_0('0', 5),
-        EXCLAMATION_POINT('!', 1),
-        AT_SYMBOL('@', 6),
-        NUM_SIGN('#', 5),
-        DOLLAR_SIGN('$', 5),
-        PERCENT('%', 5),
-        UP_ARROW('^', 5),
-        AMPERSAND('&', 5),
-        ASTERISK('*', 5),
-        LEFT_PARENTHESIS('(', 4),
-        RIGHT_PERENTHESIS(')', 4),
-        MINUS('-', 5),
-        UNDERSCORE('_', 5),
-        PLUS_SIGN('+', 5),
-        EQUALS_SIGN('=', 5),
-        LEFT_CURL_BRACE('{', 4),
-        RIGHT_CURL_BRACE('}', 4),
-        LEFT_BRACKET('[', 3),
-        RIGHT_BRACKET(']', 3),
-        COLON(':', 1),
-        SEMI_COLON(';', 1),
-        DOUBLE_QUOTE('"', 3),
-        SINGLE_QUOTE('\'', 1),
-        LEFT_ARROW('<', 4),
-        RIGHT_ARROW('>', 4),
-        QUESTION_MARK('?', 5),
-        SLASH('/', 5),
-        BACK_SLASH('\\', 5),
-        LINE('|', 1),
-        TILDE('~', 5),
-        TICK('`', 2),
-        PERIOD('.', 1),
-        COMMA(',', 1),
-        SPACE(' ', 3),
-        DEFAULT('a', 4);
-
-        private final char character;
-        private final int length;
-
-        DefaultFontInfo(char character, int length) {
-            this.character = character;
-            this.length = length;
-        }
-
-        public char getCharacter() {
-            return this.character;
-        }
-
-        public int getLength() {
-            return this.length;
-        }
-
-        public int getBoldLength() {
-            if (this == SPACE) {
-                return this.getLength();
-            }
-            return this.length + 1;
-        }
-
-        public static CenterChat.DefaultFontInfo getDefaultFontInfo(char c) {
-            for (CenterChat.DefaultFontInfo dFI : CenterChat.DefaultFontInfo.values()) {
-                if (dFI.getCharacter() == c) {
-                    return dFI;
-                }
-            }
-            return DEFAULT;
-        }
-    }
-
 }
-// :)
