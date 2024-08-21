7b504a72ed37b0989667a3e0270a46e7a571151a
hapyl <deojoff@gmail.com>
2024-08-21 12:50:54 +0500

    Deprecate BlockStudioParser in favor of BDEntity

--- src/main/java/me/hapyl/eterna/module/block/display/DisplayData.java
+++ src/main/java/me/hapyl/eterna/module/block/display/DisplayData.java
@@ -2,6 +2,7 @@ package me.hapyl.eterna.module.block.display;
 
 import com.google.common.collect.Lists;
 import me.hapyl.eterna.module.entity.Entities;
+import me.hapyl.eterna.module.util.Runnables;
 import org.bukkit.Location;
 import org.bukkit.entity.Display;
 
@@ -14,6 +15,11 @@ import java.util.function.Consumer;
  * Represents data for displays to later spawn.
  */
 public class DisplayData {
+
+    /**
+     * The default delay before teleporting the entity.
+     */
+    public static final int TELEPORTATION_DELAY = 2;
 
     private final List<DisplayDataObject<?>> objects;
 
@@ -113,6 +119,67 @@ public class DisplayData {
     public DisplayEntity spawnInterpolated(@Nonnull Location location) {
         final DisplayEntity entity = spawn(location);
         entity.setTeleportDuration(1);
+
+        return entity;
+    }
+
+    /**
+     * Spawns the displays at the given {@link Location}, accepts the {@link Consumer} and teleports to the {@link Location}.
+     * <br>
+     * <pre>{@code
+     * final DisplayData data = BDEngine.parse(...);
+     *
+     * data.spawnInterpolated(
+     *     player.getLocation(),
+     *     player.getLocation().add(0, 5, 0),
+     *     self -> {
+     *         self.setTeleportDuration(20);
+     *     }
+     * );
+     * }</pre>
+     * <br>
+     * Will spawn the entity at the player's location, and teleport it five blocks above after {@link #TELEPORTATION_DELAY} ticks over twenty ticks.
+     *
+     * @param location    - Location to spawn at.
+     * @param endLocation - Location to teleport to.
+     * @param consumer    - Consumer to accept.
+     * @return a display entity.
+     */
+    @Nonnull
+    public DisplayEntity spawnInterpolated(@Nonnull Location location, @Nonnull Location endLocation, @Nonnull Consumer<Display> consumer) {
+        return spawnInterpolated(location, endLocation, consumer, TELEPORTATION_DELAY);
+    }
+
+    /**
+     * Spawns the displays at the given {@link Location}, accepts the {@link Consumer} and teleports to the {@link Location}.
+     * <br>
+     * <pre>{@code
+     * final DisplayData data = BDEngine.parse(...);
+     *
+     * data.spawnInterpolated(
+     *     player.getLocation(),
+     *     player.getLocation().add(0, 5, 0),
+     *     self -> {
+     *         self.setTeleportDuration(20);
+     *     }, 5
+     * );
+     * }</pre>
+     * <br>
+     * Will spawn the entity at the player's location, and teleport it five blocks above after five ticks over twenty ticks.
+     *
+     * @param location      - Location to spawn at.
+     * @param endLocation   - Location to teleport to.
+     * @param consumer      - Consumer to accept.
+     * @param teleportDelay - Delay before teleporting.
+     *                      Ticks are weird, sometimes it works with <code>1</code> sometimes only with <code>2</code>, leaving this here for custom delay.
+     * @return a display entity.
+     */
+    @Nonnull
+    public DisplayEntity spawnInterpolated(@Nonnull Location location, @Nonnull Location endLocation, @Nonnull Consumer<Display> consumer, int teleportDelay) {
+        final DisplayEntity entity = spawn(location, consumer);
+
+        // Teleport a little later because it doesn't work at the same tick yep
+        Runnables.runLater(() -> entity.teleport(endLocation), teleportDelay);
 
         return entity;
     }
