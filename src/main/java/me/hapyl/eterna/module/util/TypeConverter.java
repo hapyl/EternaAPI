4eeeb80c729e5ffff2e8f17174c91e90d8c963ea
xanyjl <deojoff@gmail.com>
2025-04-14 02:24:15 +0500

    Fix #276 & add javadocs to ArgumentList

--- src/main/java/me/hapyl/eterna/module/util/TypeConverter.java
+++ src/main/java/me/hapyl/eterna/module/util/TypeConverter.java
@@ -14,13 +14,13 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class TypeConverter {
-
+    
     private final Object obj;
-
+    
     public TypeConverter(@Nonnull Object obj) {
         this.obj = Objects.requireNonNull(obj, "Convertible object cannot be null!");
     }
-
+    
     /**
      * Converts this object to a string.
      *
@@ -30,7 +30,7 @@ public class TypeConverter {
     public String toString() {
         return String.valueOf(obj);
     }
-
+    
     /**
      * Converts this object to a byte, or 0 if the object is not a byte.
      *
@@ -39,7 +39,7 @@ public class TypeConverter {
     public byte toByte() {
         return toByte((byte) 0);
     }
-
+    
     /**
      * Converts this object to a byte, or def if the object is not a byte.
      *
@@ -49,7 +49,7 @@ public class TypeConverter {
     public byte toByte(byte def) {
         return Numbers.getByte(obj, def);
     }
-
+    
     /**
      * Converts this object to a short, or 0 if the object is not a byte.
      *
@@ -58,7 +58,7 @@ public class TypeConverter {
     public short toShort() {
         return toShort((short) 0);
     }
-
+    
     /**
      * Converts this object to a short, or def if the object is not a byte.
      *
@@ -68,7 +68,7 @@ public class TypeConverter {
     public short toShort(short def) {
         return Numbers.getShort(obj, def);
     }
-
+    
     /**
      * Converts this object to an integer, or 0 if the object is not an integer.
      *
@@ -77,7 +77,7 @@ public class TypeConverter {
     public int toInt() {
         return toInt(0);
     }
-
+    
     /**
      * Converts this object to an integer, or def if the object is not an integer.
      *
@@ -87,7 +87,7 @@ public class TypeConverter {
     public int toInt(int def) {
         return Numbers.getInt(obj, def);
     }
-
+    
     /**
      * Converts this object to a long, or 0L if the object is not a long.
      *
@@ -96,7 +96,7 @@ public class TypeConverter {
     public long toLong() {
         return toLong(0);
     }
-
+    
     /**
      * Converts this object to a long, or def if the object is not a long.
      *
@@ -106,7 +106,7 @@ public class TypeConverter {
     public long toLong(long def) {
         return Numbers.getLong(obj, def);
     }
-
+    
     /**
      * Converts this object to a double, or 0 if the object is not a double.
      *
@@ -115,7 +115,7 @@ public class TypeConverter {
     public double toDouble() {
         return toDouble(0);
     }
-
+    
     /**
      * Converts this object to a double, or def if the object is not a double.
      *
@@ -125,7 +125,7 @@ public class TypeConverter {
     public double toDouble(double def) {
         return Numbers.getDouble(obj, def);
     }
-
+    
     /**
      * Converts this object to a float, or def if the object is not a float.
      *
@@ -134,7 +134,7 @@ public class TypeConverter {
     public float toFloat() {
         return toFloat(0);
     }
-
+    
     /**
      * Converts this object to a float, or def if the object is not a float.
      *
@@ -144,7 +144,7 @@ public class TypeConverter {
     public float toFloat(float def) {
         return Numbers.getFloat(obj, def);
     }
-
+    
     /**
      * Converts this object to a boolean.
      *
@@ -153,7 +153,7 @@ public class TypeConverter {
     public boolean toBoolean() {
         return "true".equalsIgnoreCase(obj.toString());
     }
-
+    
     /**
      * Converts this object to an enum constant, or null if the enum doesn't have a constant by the given name.
      *
@@ -165,7 +165,7 @@ public class TypeConverter {
     public <T extends Enum<T>> T toEnum(@Nonnull Class<T> clazz) {
         return toEnum(clazz, null);
     }
-
+    
     /**
      * Converts this object to an enum constant, or def if the enum doesn't have a constant by the given name.
      *
@@ -178,7 +178,7 @@ public class TypeConverter {
     public <T extends Enum<T>> T toEnum(@Nonnull Class<T> clazz, T def) {
         return Enums.byName(clazz, obj.toString(), def);
     }
-
+    
     /**
      * Converts this object to an online player, or null if the player is not online.
      *
@@ -188,7 +188,7 @@ public class TypeConverter {
     public Player toPlayer() {
         return Bukkit.getPlayer(toString());
     }
-
+    
     /**
      * Converts this object to an online player, or def if the player is not online.
      *
@@ -198,10 +198,10 @@ public class TypeConverter {
     @Nonnull
     public Player toPlayer(@Nonnull Player def) {
         final Player player = toPlayer();
-
+        
         return player == null ? def : player;
     }
-
+    
     /**
      * Converts this object to a {@link UUID}, or null if invalid {@link UUID}.
      *
@@ -211,11 +211,12 @@ public class TypeConverter {
     public UUID toUUID() {
         try {
             return UUID.fromString(toString());
-        } catch (IllegalArgumentException ignored0) {
+        }
+        catch (IllegalArgumentException ignored0) {
             return null;
         }
     }
-
+    
     /**
      * Converts this object to a {@link UUID}, or def if invalid {@link UUID}.
      *
@@ -225,10 +226,10 @@ public class TypeConverter {
     @Nonnull
     public UUID toUUID(@Nonnull UUID def) {
         final UUID uuid = toUUID();
-
+        
         return uuid != null ? uuid : def;
     }
-
+    
     /**
      * Converts this object into a formatted {@link String} with the given format.
      * <pre>
@@ -244,7 +245,7 @@ public class TypeConverter {
     public String toStringFormatted(@Nonnull String format) {
         return format.formatted(obj);
     }
-
+    
     /**
      * Converts this object into a {@link Key} and uses it to access the given {@link Registry} to retrieve an item.
      *
@@ -254,10 +255,10 @@ public class TypeConverter {
     @Nullable
     public <K extends Keyed> K toRegistryItem(@Nonnull Registry<K> registry) {
         final Key key = Key.ofStringOrNull(toString());
-
+        
         return key != null ? registry.get(key) : null;
     }
-
+    
     /**
      * Creates a new instance of {@link TypeConverter}.
      *
@@ -266,22 +267,42 @@ public class TypeConverter {
      * @param <T> - Type of the object.
      * @return a new instance of {@link TypeConverter}.
      */
+    @Nonnull
     public static <T> TypeConverter from(@Nullable Object any, T def) {
-        if (any == null) {
-            return new TypeConverter(def);
-        }
-
-        return new TypeConverter(any);
+        return any != null ? new TypeConverter(any) : new TypeConverter(def);
     }
-
+    
     /**
      * Creates a new instance of {@link TypeConverter}.
      *
      * @param any - Object to convert.
      * @return a new instance of {@link TypeConverter}.
      */
+    @Nonnull
     public static TypeConverter from(@Nonnull Object any) {
         return new TypeConverter(any);
     }
-
+    
+    /**
+     * Creates a new instance of {@link TypeConverter} from the given nullable object.
+     * If the object is {@code null}, an empty instance is created, a real instance is created otherwise.
+     *
+     * @param object - The object.
+     * @return a new instance of {@link TypeConverter} from the given nullable object.
+     */
+    public static TypeConverter fromNullable(@Nullable Object object) {
+        return object != null ? new TypeConverter(object) : empty();
+    }
+    
+    /**
+     * Creates a new empty instance of {@link TypeConverter}.
+     * <p>This is completely useless on its own and only used in {@link #fromNullable(Object)}</p>
+     *
+     * @return a new empty instance of {@link TypeConverter}.
+     */
+    @Nonnull
+    public static TypeConverter empty() {
+        return new TypeConverter("");
+    }
+    
 }
