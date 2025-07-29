33942cea638b44c585eb30fca2df8442b6c39910
xanyjl <deojoff@gmail.com>
2025-07-30 04:20:19 +0500

    Add TypeConverter#toKey & TypeConverter#toConvertible

--- src/main/java/me/hapyl/eterna/module/util/TypeConverter.java
+++ src/main/java/me/hapyl/eterna/module/util/TypeConverter.java
@@ -6,6 +6,7 @@ import me.hapyl.eterna.module.registry.Keyed;
 import me.hapyl.eterna.module.registry.Registry;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.ApiStatus;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -260,6 +261,28 @@ public class TypeConverter {
     }
     
     /**
+     * Converts this object into a {@link Key}.
+     *
+     * @return the key, or {@code null} if key is malformed.
+     */
+    @Nullable
+    public Key toKey() {
+        return Key.ofStringOrNull(toString());
+    }
+    
+    /**
+     * Converts this object into a custom {@link TypeConvertible}.
+     *
+     * @param convertible - The convertible.
+     * @param <T>         - The convertible type.
+     * @return an {@code T}, or {@code null}.
+     */
+    @Nullable
+    public <T> T toConvertible(@Nonnull TypeConvertible<T> convertible) {
+        return convertible.convert(obj);
+    }
+    
+    /**
      * Creates a new instance of {@link TypeConverter}.
      *
      * @param any - Object to convert.
@@ -290,6 +313,7 @@ public class TypeConverter {
      * @param object - The object.
      * @return a new instance of {@link TypeConverter} from the given nullable object.
      */
+    @Nonnull
     public static TypeConverter fromNullable(@Nullable Object object) {
         return object != null ? new TypeConverter(object) : empty();
     }
@@ -301,6 +325,7 @@ public class TypeConverter {
      * @return a new empty instance of {@link TypeConverter}.
      */
     @Nonnull
+    @ApiStatus.Internal
     public static TypeConverter empty() {
         return new TypeConverter("");
     }
