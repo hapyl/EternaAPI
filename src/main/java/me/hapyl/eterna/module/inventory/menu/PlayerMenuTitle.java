53c62cbd53a0d4a89dbc9c7fce6fd8a657705b58
xanyjl <deojoff@gmail.com>
2026-02-13 22:12:39 +0500

    Add javadoc to PlayerMenuTitle

--- src/main/java/me/hapyl/eterna/module/inventory/menu/PlayerMenuTitle.java
+++ src/main/java/me/hapyl/eterna/module/inventory/menu/PlayerMenuTitle.java
@@ -3,27 +3,70 @@ package me.hapyl.eterna.module.inventory.menu;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ComponentLike;
 import net.kyori.adventure.text.TextComponent;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a title supplier for a {@link PlayerMenu}.
+ */
 public interface PlayerMenuTitle extends ComponentLike {
     
+    /**
+     * Gets the title as a {@link Component}.
+     *
+     * @return the title as a component.
+     */
     @Override
     @NotNull
     Component asComponent();
     
+    /**
+     * Creates a {@link PlayerMenuTitle} from the given {@link Component}.
+     *
+     * <p>
+     * This method is to be considered as obsolete since it can be replaced with a lambda.
+     * </p>
+     *
+     * @param title - The title to create.
+     * @return a new player menu title.
+     */
     @NotNull
+    @ApiStatus.Obsolete
     static PlayerMenuTitle create(@NotNull Component title) {
         return () -> title;
     }
     
+    /**
+     * Creates a {@link PlayerMenuTitle} from the given {@link Component} parts into a
+     * breadcrumb-style title separated by a {@link PlayerMenuTitleImpl#SEPARATOR}.
+     *
+     * <p>
+     * Special cases:
+     * <ul>
+     *      <li>If the given {@code titles} are of length {@code 0}, an empty component is returned.
+     *      <li>If the given {@code titles} are of length {@code 1}, the first component is returned.
+     * </ul>
+     * </p>
+     *
+     * @param titles - The titles parts.
+     * @return a new player menu title.
+     */
     @NotNull
     static PlayerMenuTitle create(@NotNull Component... titles) {
+        if (titles.length == 0) {
+            return Component::empty;
+        }
+        else if (titles.length == 1) {
+            return () -> titles[0];
+        }
+        
         return new PlayerMenuTitleImpl(titles);
     }
     
     class PlayerMenuTitleImpl implements PlayerMenuTitle {
         
-        private static final Component ARROW = Component.text("➔");
+        @NotNull
+        public static final Component SEPARATOR = Component.text("➔");
         
         private final Component component;
         
@@ -34,7 +77,7 @@ public interface PlayerMenuTitle extends ComponentLike {
                 final Component component = components[i];
                 
                 if (i != 0) {
-                    builder.appendSpace().append(ARROW).appendSpace();
+                    builder.appendSpace().append(SEPARATOR).appendSpace();
                 }
                 
                 builder.append(component);
